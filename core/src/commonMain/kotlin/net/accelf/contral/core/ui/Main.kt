41691e9c21a0ebcefb01ed9a78682b2471e637a9
kyori19 <kyori@accelf.net>
2021-08-06 17:56:44 +0900

    Fix lint

--- core/src/commonMain/kotlin/net/accelf/contral/core/ui/Main.kt
+++ core/src/commonMain/kotlin/net/accelf/contral/core/ui/Main.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
