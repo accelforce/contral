2b498651e4e7e4f4582430f6e1b527792117b448
kyori19 <kyori@accelf.net>
2021-08-12 02:13:47 +0900

    Update dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -49,13 +49,13 @@ android {
 
 dependencies {
     implementation(project(":core"))
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("com.arkivanov.decompose:decompose:0.3.1")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
